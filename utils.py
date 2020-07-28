cad6050c6a73aef6c32dd3e371be5c9e177b8772
Osama Maharmeh <44961698+NullOsama@users.noreply.github.com>
2020-07-28 10:05:18 +0300

    added docstring for smooth function

--- utils.py
+++ utils.py
@@ -3,10 +3,23 @@ import numpy as numpy
 def smooth(loss, curr_loss):
     """
         Smooth the loss using exponentialy weighted averages.
+
+        Parameters
+        -----------
+        loss : Last computed loss (previous loss)
+        curr_loss : currently computed loss
+
+        Returns
+        ----------
+        smoothed_loss : float
+                            Smoothed loss across the last 1000 losses(aprox.).
     """
     beta = 0.999
     return beta * loss + (1 - beta) * curr_loss
 
 def print_sample(sample_idx, idx2char):
+    """
+        Convert indices of the sample to charecters and join them to construct a sentence.
+    """
     text = ''.join(idx2char[ch] for ch in sample_idx)
     print(f'----\n {text} \n----')
