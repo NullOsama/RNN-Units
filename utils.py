8b90abab79f9b8c2b188d6762d02821ddca6b0b6
Osama Maharmeh <44961698+NullOsama@users.noreply.github.com>
2020-07-28 10:26:23 +0300

    implemented rnn_forward

--- utils.py
+++ utils.py
@@ -140,3 +140,31 @@ def update_parameters(parameters, gradients, lr):
     parameters['b']  += -lr * gradients['db']
     parameters['by']  += -lr * gradients['dby']
     return parameters
+
+def rnn_forward(X, Y, a0, parameters, vocab_size = 71):
+    """
+        Perform a forward pass on the network.
+    """
+    # Initialize x, a and y_hat as empty dictionaries
+    x, a, y_hat = {}, {}, {}
+    
+    a[-1] = np.copy(a0)
+    
+    # initialize your loss to 0
+    loss = 0
+    
+    for t in range(len(X)):
+        
+        # Set x[t] to be the one-hot vector representation of the t'th character in X.
+        x[t] = np.zeros((vocab_size,1)) 
+        x[t][X[t]] = 1
+        
+        # Run one step forward of the RNN
+        a[t], y_hat[t] = rnn_step_forward(parameters, a[t-1], x[t])
+        
+        # Update the loss by substracting the cross-entropy term of this time-step from it.
+        loss -= np.log(y_hat[t][Y[t],0])
+        
+    cache = (y_hat, a, x)
+        
+    return loss, cache
