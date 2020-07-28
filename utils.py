d909f011ce9780774728baf8001a913f47d73158
Osama Maharmeh <44961698+NullOsama@users.noreply.github.com>
2020-07-28 10:01:01 +0300

    implemented print sample function

--- utils.py
+++ utils.py
@@ -6,3 +6,7 @@ def smooth(loss, curr_loss):
     """
     beta = 0.999
     return beta * loss + (1 - beta) * curr_loss
+
+def print_sample(sample_idx, idx2char):
+    text = ''.join(idx2char[ch] for ch in sample_idx)
+    print(f'----\n {text} \n----')
