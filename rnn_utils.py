c28f1d4c2254e45ab437b78d00f8f62891fedf4c
Osama Maharmeh <44961698+NullOsama@users.noreply.github.com>
2020-07-28 09:31:58 +0300

    implemented softmax function

---
+++ rnn_utils.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+
+def softmax(x):
+    """
+        Implementation of the softmax function.  used the max trick so the values stay small and the exp function don't explod.
+
+        Parameters
+        ----------
+        x : vector of shape (C,)
+
+        Returns
+        ----------
+        numpy.ndarray
+                        Softmax of the input vector so all values are less than one and sum up to one.
+    """
+    x_max = np.max(x)
+    return np.exp(x - x_max) / np.sum(np.exp(x - x_max))
