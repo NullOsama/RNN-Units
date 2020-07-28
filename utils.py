fd6436134b158b6f79157a865a5e2a5ae0a1c52f
Osama Maharmeh <44961698+NullOsama@users.noreply.github.com>
2020-07-28 10:10:53 +0300

    Imported needed packages

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+from rnn_utils import softmax
 
 def smooth(loss, curr_loss):
     """
@@ -49,3 +50,11 @@ def initialize_parameters(n_a, n_x, n_y):
     parameters = {"Wax": Wax, "Waa": Waa, "Wya": Wya, "b": b,"by": by}
 
     return parameters
+
+def rnn_step_forward(parameters, a_prev, x):
+    
+    Waa, Wax, Wya, by, b = parameters['Waa'], parameters['Wax'], parameters['Wya'], parameters['by'], parameters['b']
+    a_next = np.tanh(np.dot(Wax, x) + np.dot(Waa, a_prev) + b) # hidden state
+    p_t = softmax(np.dot(Wya, a_next) + by) # unnormalized log probabilities for next chars # probabilities for next chars 
+    
+    return a_next, p_t
