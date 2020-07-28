34867f9128e05f441c3ac29e04846a39982ae506
Osama Maharmeh <44961698+NullOsama@users.noreply.github.com>
2020-07-28 09:58:22 +0300

    add docstring to the smooth function

--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
 import numpy as numpy
 
 def smooth(loss, curr_loss):
+    """
+        Smooth the loss using exponentialy weighted averages.
+    """
     beta = 0.999
     return beta * loss + (1 - beta) * curr_l
