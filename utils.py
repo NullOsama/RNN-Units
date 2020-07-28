b5a34fa25f824875e2f1c21a4020b0968121bf13
Osama Maharmeh <44961698+NullOsama@users.noreply.github.com>
2020-07-28 09:58:58 +0300

    fixed typo line 8

--- utils.py
+++ utils.py
@@ -5,4 +5,4 @@ def smooth(loss, curr_loss):
         Smooth the loss using exponentialy weighted averages.
     """
     beta = 0.999
-    return beta * loss + (1 - beta) * curr_l
+    return beta * loss + (1 - beta) * curr_loss
