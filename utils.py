1b6424ae4c425a9c91c5c2fc991d546d5d8ceed0
Osama Maharmeh <44961698+NullOsama@users.noreply.github.com>
2020-07-28 10:26:58 +0300

    implemented rnn_backward function

--- utils.py
+++ utils.py
@@ -168,3 +168,29 @@ def rnn_forward(X, Y, a0, parameters, vocab_size = 71):
     cache = (y_hat, a, x)
         
     return loss, cache
+
+def rnn_backward(X, Y, parameters, cache):
+    """
+        Perform a backward pass on the network.
+    """
+    # Initialize gradients as an empty dictionary
+    gradients = {}
+    
+    # Retrieve from cache and parameters
+    (y_hat, a, x) = cache
+    Waa, Wax, Wya, by, b = parameters['Waa'], parameters['Wax'], parameters['Wya'], parameters['by'], parameters['b']
+    
+    # each one should be initialized to zeros of the same dimension as its corresponding parameter
+    gradients['dWax'], gradients['dWaa'], gradients['dWya'] = np.zeros_like(Wax), np.zeros_like(Waa), np.zeros_like(Wya)
+    gradients['db'], gradients['dby'] = np.zeros_like(b), np.zeros_like(by)
+    gradients['da_next'] = np.zeros_like(a[0])
+    
+    ### START CODE HERE ###
+    # Backpropagate through time
+    for t in reversed(range(len(X))):
+        dy = np.copy(y_hat[t])
+        dy[Y[t]] -= 1
+        gradients = rnn_step_backward(dy, gradients, parameters, x[t], a[t], a[t-1])
+    ### END CODE HERE ###
+    
+    return gradients, a
