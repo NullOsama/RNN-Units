e9c5961655d60c3cd2563b0001411f9bcf49508d
Osama Maharmeh <44961698+NullOsama@users.noreply.github.com>
2020-07-28 09:37:49 +0300

    implemented initialize_adam function

--- rnn_utils.py
+++ rnn_utils.py
@@ -25,3 +25,40 @@ def sigmoid(x):
         Implementation of the softmax function.
     """
     return 1.0 / (1 + np.exp(-x))
+
+
+def initialize_adam(parameters):
+    """
+    Initializes v and s as two python dictionaries with:
+                - keys: "dW1", "db1", ..., "dWL", "dbL" 
+                - values: numpy arrays of zeros of the same shape as the corresponding gradients/parameters.
+    
+    Arguments:
+    parameters -- python dictionary containing the parameters.
+                    parameters["W" + str(l)] = Wl
+                    parameters["b" + str(l)] = bl
+    
+    Returns: 
+    v -- python dictionary that will contain the exponentially weighted average of the gradient.
+                    v["dW" + str(l)] = ...
+                    v["db" + str(l)] = ...
+    s -- python dictionary that will contain the exponentially weighted average of the squared gradient.
+                    s["dW" + str(l)] = ...
+                    s["db" + str(l)] = ...
+
+    """
+
+    L = len(parameters) // 2 # Number of layers in teh network
+
+    v = {}
+    s = {}
+
+    # Initialize v, s. Input: "parameters". Outputs: "v, s".
+    for l in range(L):
+        v['dW' + str(l)] = np.zeros(parameters['W' + str(l)].shape)
+        v['db' + str(l)] = np.zeros(parameters['b' + str(l)].shape)        
+        s["dW" + str(l+1)] = np.zeros(parameters["W" + str(l+1)].shape)
+        s["db" + str(l + 1)] = np.zeros(parameters["b" + str(l + 1)].shape)
+        
+    return v, s
+
